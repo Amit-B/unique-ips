459fba1567151d58bacffaaf4bc08ba1c24bfffc
AmitBarami <amitbarami@hotmail.com>
2018-01-16 13:44:55 +0200

    Initial commit

---
+++ UniqueIPList/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace UniqueIPList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.MaxLength = 99999999;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(304, 504);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Just IPs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(514, 12);
+            this.textBox2.MaxLength = 32767000;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(304, 504);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(322, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Find Mul. IPs";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(322, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Linux Lines Fix";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(322, 128);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Drop Firewall List";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(322, 186);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(186, 306);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(322, 99);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(134, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Find Mul. IPs (mul-thread)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(462, 102);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(322, 503);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Unique IPs found = 0";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(462, 496);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(322, 157);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(186, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "CSF List";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 528);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ UniqueIPList/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace UniqueIPList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> ips = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.ResetText();
+            for (int i = 0; i < textBox1.Lines.Length; i++)
+            {
+                var match = Regex.Match(textBox1.Lines[i], @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                if (match.Success)
+                    if (!ips.Contains(match.Captures[0].Value))
+                        ips.Add(match.Captures[0].Value);
+            }
+            for (int i = 0; i < ips.Count; i++)
+                textBox2.Text += ips[i] + "\r\n";
+            label3.Text = "Unique IPs found = " + ips.Count;
+        }
+        public static Hashtable fwips = new Hashtable();
+        public bool thrRun = false;
+        public static int total = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.ResetText();
+            fwips.Clear();
+            /*Parallel.For(0, textBox1.Lines.Length, i =>
+            {
+                button2.Text = "A: " + i + " / " + textBox1.Lines.Length;
+                Refresh();
+                if (textBox1.Lines[i].Contains("length 4"))
+                {
+                    var match = Regex.Match(textBox1.Lines[i], @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                    if (match.Success)
+                        if (match.Value != "212.29.229.4")
+                        {
+                            if (fwips.ContainsKey(match.Value))
+                                fwips[match.Value] = Convert.ToInt32(fwips[match.Value]) + 1;
+                            else
+                                fwips.Add(match.Value, 1);
+                        }
+                }
+            });*/
+            /*int jj = 0;
+            Parallel.ForEach(textBox1.Lines, i =>
+            {
+                button2.Text = "A: " + (++jj) + " / " + textBox1.Lines.Length;
+                Refresh();
+                if (i.Contains("length 4"))
+                {
+                    var match = Regex.Match(i, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                    if (match.Success)
+                        if (match.Value != "212.29.229.4")
+                        {
+                            if (fwips.ContainsKey(match.Value))
+                                fwips[match.Value] = Convert.ToInt32(fwips[match.Value]) + 1;
+                            else
+                                fwips.Add(match.Value, 1);
+                        }
+                }
+            });*/
+            for (int i = 0; i < textBox1.Lines.Length; i++)
+            {
+                //20:30:53.614466 IP 24.78.87.141.45425 > 212.29.229.4.cbt: UDP, length 4
+                button2.Text = "A: " + i + " / " + textBox1.Lines.Length;
+                Refresh();
+                var match = Regex.Match(textBox1.Lines[i], @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                if (match.Success)
+                {
+                    if (match.Value == "212.29.229.4")
+                        continue;
+                    if (fwips.ContainsKey(match.Value))
+                    {
+                        fwips[match.Value] = Convert.ToInt32(fwips[match.Value]) + 1;
+                        //MessageBox.Show("added to " + match.Value + ", " + fwips[match.Value]);
+                    }
+                    else
+                    {
+                        fwips.Add(match.Value, 1);
+                        //MessageBox.Show("created " + match.Value);
+                    }
+                }
+                //else
+                    //MessageBox.Show("No IP");
+            }
+            //MessageBox.Show(fwips.Count.ToString());
+            string fieldName = string.Empty;
+            foreach (DictionaryEntry de in fwips)
+            {
+                fieldName = de.Key as string;
+                if (Convert.ToInt32(de.Value) >= (int)numericUpDown2.Value)
+                    textBox2.Text += fieldName + "=" + de.Value + "\r\n";
+            }
+            label3.Text = "Unique IPs found = " + fwips.Count.ToString();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!thrRun)
+            {
+                thrRun = true;
+                total = 0;
+                textBox2.ResetText();
+                fwips.Clear();
+                int TIMES = (int)numericUpDown1.Value;
+                ManualResetEvent[] doneEvents = new ManualResetEvent[TIMES];
+                ReadIPs[] array = new ReadIPs[TIMES];
+                textBox3.Text = "Starting " + TIMES + " threads\r\n";
+                int div = textBox1.Lines.Length / TIMES;
+                for (int i = 0; i < TIMES; i++)
+                {
+                    doneEvents[i] = new ManualResetEvent(false);
+                    ReadIPs f = new ReadIPs(i * div, ((i + 1) * div) > textBox1.Lines.Length ? textBox1.Lines.Length : ((i + 1) * div), textBox1.Lines, doneEvents[i], textBox3);
+                    array[i] = f;
+                    ThreadPool.QueueUserWorkItem(f.ThreadPoolCallback, i);
+                }
+                //WaitHandle.WaitAll(doneEvents);
+                foreach (var ee in doneEvents)
+                    ee.WaitOne();
+                textBox3.Text += "Finished all";
+                string fieldName = string.Empty;
+                //MessageBox.Show(fwips.Count.ToString());
+                foreach (DictionaryEntry de in fwips)
+                {
+                    fieldName = de.Key as string;
+                    if (Convert.ToInt32(de.Value) >= (int)numericUpDown2.Value)
+                        textBox2.Text += fieldName + "=" + de.Value + "\r\n";
+                }
+                label3.Text = "Unique IPs found = " + fwips.Count.ToString();
+                thrRun = false;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Replace("\n", "\r\n");
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // iptables -A INPUT -s 1337 -j DROP
+            string lines = string.Empty;
+            for (int i = 0; i < textBox2.Lines.Length; i++)
+                lines += "iptables -A INPUT -s " + textBox2.Lines[i].Split('=')[0] + " -j DROP\r\n";
+            textBox2.Text = lines;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string lines = string.Empty;
+            for (int i = 0; i < textBox2.Lines.Length; i++)
+                lines += textBox2.Lines[i].Split('=')[0] + "\r\n";
+            textBox2.Text = lines;
+        }
+        public class ReadIPs
+        {
+            private int start = 0, end = 0;
+            private string[] lines;
+            private ManualResetEvent ev;
+            private TextBox logtb;
+            public ReadIPs(int start, int end, string[] lines, ManualResetEvent ev, TextBox logtb)
+            {
+                this.start = start;
+                this.end = end;
+                this.ev = ev;
+                this.lines = lines;
+                this.logtb = logtb;
+            }
+            public void ThreadPoolCallback(Object threadContext)
+            {
+                int threadIndex = (int)threadContext;
+                Log("Thread " + threadIndex + " started");
+                Read();
+                Log("Thread " + threadIndex + " finished");
+                ev.Set();
+            }
+            public void Read()
+            {
+                //MessageBox.Show("Start = " + start + ", end = " + end);
+                for (int i = start; i < end && i < lines.Length; i++)
+                {
+                    var match = Regex.Match(lines[i], @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                    if (match.Success)
+                    {
+                        if (match.Value == "212.29.229.4")
+                            continue;
+                        if (fwips.ContainsKey(match.Value))
+                            fwips[match.Value] = Convert.ToInt32(fwips[match.Value]) + 1;
+                        else
+                            fwips.Add(match.Value, 1);
+                        //MessageBox.Show(fwips.Count.ToString() + " new");
+                        total++;
+                    }
+                }
+            }
+            public void Log(string text)
+            {
+                /*if (text.Length == 0)
+                {
+                    if (logtb.InvokeRequired == true)
+                        logtb.Invoke((MethodInvoker)delegate
+                        {
+                            logtb.ResetText();
+                        });
+                    else
+                        logtb.ResetText();
+                }
+                else
+                {
+                    if (logtb.InvokeRequired == true)
+                        logtb.Invoke((MethodInvoker)delegate
+                        {
+                            logtb.Text += text + "\r\n";
+                        });
+                    else
+                        logtb.Text += text + "\r\n";
+                }*/
+            }
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (thrRun)
+                button5.Text = total + " / " + textBox1.Lines.Length;
+        }
+        public void AppendTextBox(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox), new object[] { value });
+                return;
+            }
+            ActiveForm.Text += value;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+    }
+}
